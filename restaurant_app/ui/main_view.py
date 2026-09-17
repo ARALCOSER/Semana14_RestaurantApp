52f25819b34e2cd1edfdff9328e105d15697e564
RAlcoser <ar.alcoser@gmail.com>
2026-09-17 16:16:14 -0500

    Restaurante Tamaño

--- restaurant_app/ui/main_view.py
+++ restaurant_app/ui/main_view.py
@@ -138,7 +138,7 @@ class MainView(tk.Frame):
             text="RESTAURANTE",
             bg=self.color_encabezado,
             fg="#ffffff",
-            font=("Arial", 17, "bold"),
+            font=("Arial", 16, "bold"),
         ).pack(anchor="w", pady=(0, 8))
 
         tk.Label(
